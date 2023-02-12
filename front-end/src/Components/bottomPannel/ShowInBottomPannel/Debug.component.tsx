ee4faa6289d93cf95e4dc98ec133f685c0cfcc1e
Vicky Gupta <vickyguptaa7@gmail.com>
2023-02-12 21:59:06 +0530

    scrolling issue fixed of bottom pannel showin bottom pannel container by fixing the height hight of bottom pannel

--- front-end/src/Components/bottomPannel/ShowInBottomPannel/Debug.component.tsx
+++ front-end/src/Components/bottomPannel/ShowInBottomPannel/Debug.component.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const Debug = () => {
   return (
-    <div className='text-white'>Debug</div>
-  )
-}
+    <div className="w-full h-full text-white">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, non enim
+      illo optio facilis officia. Ipsa enim nobis sapiente asperiores,
+      voluptates rerum mollitia eos eaque similique alias nesciunt labore et
+      totam natus officiis esse, unde reiciendis corrupti quibusdam repellendus
+      consequuntur suscipit id error voluptatum. Rem dolor porro asperiores
+      laboriosam officiis, cumque recusandae iure corporis iusto explicabo
+      consequatur! Asperiores sit aperiam eaque deserunt! Tenetur reiciendis
+      similique, iste quidem pariatur tempore, porro voluptatem ab laboriosam
+      accusantium expedita itaque asperiores aut quo veniam tempora, nemo omnis
+      amet hic architecto quam. Praesentium possimus maxime deserunt illo enim
+      vero incidunt quo quasi nisi quia minima consequatur cum quaerat
+      asperiores, aliquid nobis sint adipisci quam repudiandae velit! Vitae
+      repellendus iste sint totam officiis sunt nam soluta ratione enim quo,
+      sapiente sit quos maiores labore pariatur veniam nobis animi? Inventore,
+      optio ea aperiam, et perferendis, quibusdam in fuga eum molestias eaque
+      illum delectus placeat soluta alias quidem ipsum quam minus! Facere
+      cupiditate ullam rem earum est natus vero ex hic odit doloremque, maxime
+      fuga recusandae nesciunt vitae non corporis labore laborum et facilis. Et
+      quo totam dolor incidunt nam eos obcaecati, ducimus ut cupiditate quae
+      quisquam voluptatum excepturi molestias molestiae iusto nobis! At corporis
+      dolorum nobis officia dolorem. Repudiandae dolorem cupiditate ex non
+      similique minus adipisci cum fugit, recusandae architecto beatae ipsum
+      aliquid neque accusantium dignissimos, nam officiis animi inventore facere
+      illum molestiae velit quam. Autem, maxime. Tempore, eum obcaecati.
+      Architecto corporis ullam unde ratione quidem non dolores amet culpa
+      molestiae rerum vero, ipsam vitae illum sequi quas! Architecto accusamus,
+      delectus ullam corrupti, quod corporis, tempora molestias aperiam iste
+      suscipit voluptatum! Asperiores, dicta sit? Blanditiis animi reprehenderit
+      vitae ipsam ullam voluptatum itaque fugit quasi incidunt veritatis natus
+      dolor sequi quo numquam, quia libero tempora possimus quibusdam corrupti
+      similique tenetur soluta repellat modi! Velit a tempore tempora quidem eos
+      nam hic ratione maiores totam ex, ipsa dicta alias amet nostrum
+      voluptatibus debitis neque eligendi. Eius quos, similique labore
+      exercitationem suscipit modi? Cumque unde, molestias fugiat maxime, est
+      doloribus repellat in fuga voluptas dolorem enim mollitia? Voluptates,
+      eius? Officiis ab odio harum velit quasi repellat neque ad nobis explicabo
+      suscipit temporibus, impedit consequatur reiciendis, earum asperiores quo
+      dolores? Provident, totam accusamus sit quia quos itaque aliquam
+      consequuntur a? Amet magnam exercitationem pariatur a tempora, ratione
+      velit est molestias. Eos, perspiciatis rem? Harum necessitatibus ut
+      obcaecati possimus, eaque tempore! Dolor impedit sit voluptatum animi
+      tempore aspernatur ut dolores? A obcaecati veniam velit ducimus ea enim
+      quia omnis praesentium ipsum temporibus unde neque beatae eos, alias
+      doloribus id numquam quasi debitis minima quo possimus corporis labore
+      distinctio cumque. Rerum velit, culpa reiciendis autem atque enim quos
+      obcaecati est dolores amet deleniti voluptates rem accusantium. Doloremque
+      dolor odit assumenda est porro neque mollitia aut dignissimos eum
+      consequuntur perspiciatis ut, quibusdam sequi recusandae magni
+      necessitatibus voluptate ea deleniti et iure atque expedita dicta! Quo
+      obcaecati, optio illum accusantium asperiores fugiat earum accusamus
+      beatae sequi! Architecto, commodi dolorem. Mollitia assumenda harum ab
+      numquam maiores temporibus nostrum nesciunt modi ut, ex nisi consequuntur
+      consequatur optio sunt dolorem cum quaerat maxime! Amet facilis ex ipsam
+      officiis! Optio, vero.
+    </div>
+  );
+};
 
-export default Debug
+export default Debug;
--- front-end/src/Components/bottomPannel/ShowInBottomPannel/showInBottomPannel.component.tsx
+++ front-end/src/Components/bottomPannel/ShowInBottomPannel/showInBottomPannel.component.tsx
@@ -5,14 +5,27 @@ import Input from "./Input.component";
 import Output from "./Output.component";
 import Terminal from "./Terminal.component";
 
+//constant
+import { BOTTOM_PANNEL_NAVIGATION_HEIGHT_SIZE_PX } from "../BottomPannel.Constant";
+
 const ShowInBottomPannel = () => {
+  const bottomPannelHeight = useAppSelector(
+    (state) => state.bottomPannel.bottomPannelHeight
+  );
   const showInBottomPannel = useAppSelector(
     (state) => state.bottomPannel.showInBottomPannel
   );
   if (showInBottomPannel === "input") return <Input />;
   if (showInBottomPannel === "output") return <Output />;
   if (showInBottomPannel === "terminal") return <Terminal />;
-  return <Debug />;
+  return (
+    <div
+      className="mx-4 mt-2 overflow-scroll "
+      style={{ height: bottomPannelHeight - BOTTOM_PANNEL_NAVIGATION_HEIGHT_SIZE_PX }}
+    >
+      <Debug />;
+    </div>
+  );
 };
 
 export default ShowInBottomPannel;
