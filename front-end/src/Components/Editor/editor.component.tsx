441664569f6e7aa1fa87fcee194c3f168d3a3d54
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-02-24 14:33:21 +0530

    added monaco editor

--- front-end/src/Components/Editor/editor.component.tsx
+++ front-end/src/Components/Editor/editor.component.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import MonacoEditor from "@monaco-editor/react";
+import { useAppSelector } from "../../Store/store";
 
 const Editor = () => {
-  return <div className="bg-[color:var(--codeeditor-color)]  code-here grow"></div>;
+  const currentNavFile = useAppSelector(
+    (state) => state.fileNavigation.currentNavFile
+  );
+  console.log(currentNavFile);
+
+  return (
+    <MonacoEditor
+      language="javascript"
+      theme="vs-dark"
+      options={{
+        wordWrap: "on",
+        lineNumbersMinChars: 3, // for the line numbers at the left
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+      className="bg-[color:var(--codeeditor-color)]  code-here grow"
+      value={""}
+    ></MonacoEditor>
+  );
 };
 
 export default Editor;
--- front-end/src/Store/reducres/File/Files.reducer.tsx
+++ front-end/src/Store/reducres/File/Files.reducer.tsx
@@ -1,2 +1,2 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+// TODO:Central file state
