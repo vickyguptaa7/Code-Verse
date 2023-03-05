b5e03ded81aa78609e8d9aa6b1997cecde56ddd2
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-03-05 20:44:40 +0530

    file icons applied and folder icons also done with imporoved extension detection of files

--- front-end/src/firebase/firebaseConfig.tsx
+++ front-end/src/firebase/firebaseConfig.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
