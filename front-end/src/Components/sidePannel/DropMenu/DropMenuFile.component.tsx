6daf9424d8cbe8d5d817247128f40147dbd1da6a
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-03-26 13:35:44 +0530

    multiple files upload working

--- front-end/src/Components/sidePannel/DropMenu/DropMenuFile.component.tsx
+++ front-end/src/Components/sidePannel/DropMenu/DropMenuFile.component.tsx
@@ -13,7 +13,14 @@ import {
   addFileOrFolderToDirectory,
   updateFileBody,
 } from "../../../Store/reducres/SideDrawer/Directory/Directory.reducer";
-
+declare module "react" {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    // extends React's HTMLAttributes
+    directory?: string;
+    webkitdirectory?: string;
+    mozdirectory?: string;
+  }
+}
 interface IPROPS {
   closeDropMenuHandler: Function;
 }
@@ -39,56 +46,87 @@ export const DropMenuFile: React.FC<IPROPS> = ({ closeDropMenuHandler }) => {
     closeDropMenuHandler();
   };
 
-  // TODO : Add local files
-  const openFileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    const fileName = e.target.value.split("\\").pop()?.split(".");
+  const fileNameGenerator = (name: string) => {
+    const fileName = name.split("\\").pop()?.split(".");
     const extension = fileName ? fileName.pop() : "";
     const id: string = uuid4();
     fileName?.push(id);
     fileName?.push(extension ? extension : "");
     const newFileName = fileName ? fileName.join(".") : id;
-    console.log(newFileName);
-
-    const file = e.target.files
-      ? e.target.files[0]
-      : new File(["Something went wrong while opening the file"], "error.text");
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = () => {
-      dispatch(
-        addFileOrFolderToDirectory({
-          id: id,
-          parentId: "root",
-          name: newFileName,
-          isFolder: false,
-        })
-      );
-      dispatch(updateFileBody({ id: id, body: reader.result?reader.result.toString():"" }));
-      dispatch(addFileToNavigation({ id: id, type: "file" }));
-      console.log(reader.result?.toString());
-    };
-    reader.onerror = () => {
-      closeDropMenuHandler();
-      console.log("file error", reader.error);
-    };
-    closeDropMenuHandler();
+    return { fileName: newFileName, id };
   };
+
+  // TODO : Add local files
+  const openFileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+    closeDropMenuHandler();
+    const files = e.target.files;
+    for (const fileKey in files) {
+      if (isNaN(parseInt(fileKey))) continue;
+      const { fileName, id } = fileNameGenerator(files[parseInt(fileKey)].name);
+      console.log(files[parseInt(fileKey)]);
+      const reader = new FileReader();
+      reader.readAsText(files[parseInt(fileKey)]);
+      reader.onload = () => {
+        dispatch(
+          addFileOrFolderToDirectory({
+            id: id,
+            parentId: "root",
+            name: fileName,
+            isFolder: false,
+          })
+        );
+        dispatch(
+          updateFileBody({
+            id: id,
+            body: reader.result ? reader.result.toString() : "",
+          })
+        );
+        dispatch(addFileToNavigation({ id: id, type: "file" }));
+        // console.log(reader.result?.toString());
+      };
+      reader.onerror = () => {
+        console.log("file error", reader.error);
+      };
+    }
+  };
+
+  const openFolderHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    console.log(e.target.files);
+    
+  }
 
   return (
     <DropMenu className="w-36 -top-[54px] left-14">
       <label htmlFor="file" title="Add local files">
         <div className="cursor-pointer whitespace-nowrap block mx-1 my-0.5 px-4 py-0.5 text-sm text-start rounded-md hover:bg-[color:var(--hover-text-color)]">
-          <h1>Open File</h1>
+          <h1>Open Files</h1>
         </div>
       </label>
       <input
         type="file"
         id="file"
         name="file"
+        multiple
         className="hidden"
         onChange={openFileHandler}
       />
+      
+      <label htmlFor="folder" title="Add local files">
+        <div className="cursor-pointer whitespace-nowrap block mx-1 my-0.5 px-4 py-0.5 text-sm text-start rounded-md hover:bg-[color:var(--hover-text-color)]">
+          <h1>Open Folder</h1>
+        </div>
+      </label>
+      <input
+        type="file"
+        id="folder"
+        name="folder"
+        directory=""
+        webkitdirectory=""
+        className="hidden"
+        onChange={openFolderHandler}
+      />
+      
       <DropMenuButton name="Save Files" onClickHandler={onClickHandler} />
       <div className="w-4/5 mx-auto h-[0.5px] bg-[color:var(--primary-text-color)] my-1"></div>
       <DropMenuButton
