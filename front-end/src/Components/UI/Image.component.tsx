f53f55545557a0d0d4ba7f60af39ce52ae452f43
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-04-26 13:54:35 +0530

    fixed image src undefined

--- front-end/src/Components/UI/Image.component.tsx
+++ front-end/src/Components/UI/Image.component.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { mergeClass } from "../../library/tailwindMerge/tailwindMerge.lib";
 
 interface IPROPS extends React.ImgHTMLAttributes<HTMLImageElement> {
   fallback: React.ReactNode;
 }
 
-const Image: React.FC<IPROPS> = ({ fallback, className, alt, ...props }) => {
+const Image: React.FC<IPROPS> = ({
+  fallback,
+  className,
+  src,
+  alt,
+  ...props
+}) => {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
+  useEffect(() => {
+    if (!src) setIsImageLoaded(false);
+    else setIsImageLoaded(true);
+  }, [src]);
   return (
     <>
       {!isImageLoaded ? fallback : null}
@@ -14,9 +24,10 @@ const Image: React.FC<IPROPS> = ({ fallback, className, alt, ...props }) => {
         onLoad={() => {
           setIsImageLoaded(true);
         }}
+        src={src}
         className={mergeClass([className, !isImageLoaded ? "hidden" : ""])}
         {...props}
-        alt={alt}
+        alt={""}
       />
     </>
   );
