d0d3e4b528776720c3d973789d94ca997268be7e
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-04-29 13:07:58 +0530

    set the timer for the running js code with resource limits

--- back-end/src/utils/executeJsCode.ts
+++ back-end/src/utils/executeJsCode.ts
@@ -1,18 +1,24 @@
 import Docker from "dockerode";
+import fs from "fs";
 
 const docker = new Docker();
 
 export const executeJsCode = async (code: string) => {
+  fs.appendFileSync(`${__dirname}/code.js`, code);
+
   const container = await docker.createContainer({
     Image: "node",
     WorkingDir: "/usr/src/app",
     Tty: true,
     HostConfig: {
-      Binds: [`${__dirname}/file.js:/usr/src/app/file.js`],
+      Binds: [
+        `${__dirname}/file.js:/usr/src/app/file.js`,
+        `${__dirname}/code.js:/usr/src/app/code.js`,
+      ],
     },
     Cmd: ["node", "file.js"],
   });
-
+  let output = "";
   container.attach(
     { stream: true, stdout: true, stderr: true },
     (err, stream) => {
@@ -22,15 +28,19 @@ export const executeJsCode = async (code: string) => {
       }
       if (stream)
         stream.on("data", (data) => {
+          output += data.toString();
           console.log("container : ", data.toString());
         });
     }
   );
-  await container.start();
-  await container.wait();
+
   try {
+    await container.start();
+    await container.wait();
   } catch (err) {
     console.log(err);
   }
+  fs.unlinkSync(`${__dirname}/code.js`);
   await container.remove();
+  return output ;
 };
--- back-end/src/utils/file.js
+++ back-end/src/utils/file.js
@@ -1,10 +1,50 @@
-const fs = require('fs');
+const { spawn } = require("child_process");
 
-fs.readdir('.', (err, files) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(files);
-    console.log('Hello');
-});
+const execute = async () => {
+  return new Promise((resolve, reject) => {
+    const child = spawn("node", ["code.js"], {
+      stdio: "pipe",
+      timeout: 2000,
+      resourceLimits: {
+        maxOldGenerationSizeMb: 256,
+      },
+    });
+    let output = "";
+    child.stdout.on("data", (data) => {
+      output += data.toString().trim();
+    });
+
+    child.stderr.on("data", (data) => {
+      reject(new Error(data.toString()));
+    });
+
+    const timerId = setTimeout(() => {
+      child.kill();
+      reject(new Error("Process timed out"));
+    }, 2000);
+
+    child.on("close", (code) => {
+      if (code === 0) {
+        resolve(output);
+      } else {
+        reject(new Error(`Process exited with code ${code}`));
+      }
+      clearTimeout(timerId);
+    });
+
+    child.on("error", (err) => {
+      reject(err);
+      clearTimeout(timerId);
+    });
+  });
+};
+
+const run = async () => {
+  try {
+    const output = await execute();
+    console.log(output);
+  } catch (err) {
+    console.log(err);
+  }
+};
+run();
