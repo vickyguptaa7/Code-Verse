b1b3539e0fb8b8bf3bc7198b1cf65e3b810b7ef0
vickyguptaa7 <vickyguptaa7@gmail.com>
2023-05-05 12:08:37 +0530

    added ping to make server active as server bcom inactive in 15 minutes on render

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,9 +1,11 @@
+import axios from "axios";
 import dotenv from "dotenv";
 import express from "express";
 import helment from "helmet";
 
 import { validateApiKey } from "./middlewares/validateApiKey.middleware";
 import codeExecuteRoute from "./routes/CodeExecute.route";
+import pingRoute from "./routes/Ping.route";
 
 dotenv.config();
 
@@ -13,9 +15,29 @@ const app = express();
 app.use(helment());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(validateApiKey);
 
+app.use("/api/ping", pingRoute);
 app.use("/api/execute", codeExecuteRoute);
+
+// Ping the server every 10 minutes to keep it awake for reducing cold start time
+const TEN_MINUTES = 10 * 60 * 1000; // 10 minutes in milliseconds
+const pingUrl="https://vscode-s.onrender.com/api/ping";
+setInterval(async () => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: pingUrl,
+      headers: {
+        "x-api-key": process.env.API_KEY,
+      },
+    });
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+}, TEN_MINUTES);
 
 app.listen(port, () => {
   console.log("now listending on port", port);
