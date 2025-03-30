faa8cc774dd89d70dbfc03353d68bcbedb9689f5
vickyguptaa7 <vickyguptaa7@gmail.com>
2025-03-30 15:12:08 +0530

    backend changes with queue implementation + cors * for testing

--- back-end/src/utils/validationSchema.ts
+++ back-end/src/utils/validationSchema.ts
@@ -12,6 +12,10 @@ const codeSubmissionSchema = yup.object().shape({
   input: yup.string().trim(),
 });
 
+const codeSubmissionOutputSchema = yup.object().shape({
+  id: yup.string().trim().required("ID is required"),
+});
+
 export type TLanguage = "cpp" | "java" | "python" | "c" | "javascript";
 
 export interface ICodeSubmission
@@ -20,6 +24,26 @@ export interface ICodeSubmission
   code: string;
   language: TLanguage;
   input: string;
+  id?: string;
 }
 
-export { codeSubmissionSchema };
+export interface ISubmissionQueueItem extends ICodeSubmission {
+  id: string;
+}
+
+export interface ISubmissionDatabase {
+  [key: string]: {
+    status: "success" | "error";
+    id: string;
+    output: any;
+    error?: any;
+    timestamp: Date;
+    executionTime: number;
+  };
+}
+
+export interface IQueueIdSet {
+  [key: string]: boolean;
+}
+
+export { codeSubmissionOutputSchema, codeSubmissionSchema };
